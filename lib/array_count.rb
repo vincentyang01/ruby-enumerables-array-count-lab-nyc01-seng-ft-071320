125a8ecbd71132999798cdb4d286229fb3bc0e69
Vincent Yang <vincentyangcsi@gmail.com>
2020-07-04 22:03:15 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,12 +1,10 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   i = 0;
+  tfValues = []
   array.count do |element|
     element.class == String
-    i += 1;
-    
   end
-  i
 end
 
 def count_empty_strings(array)
